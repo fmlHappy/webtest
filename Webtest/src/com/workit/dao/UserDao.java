0cdc5b95d94b1aa5f1fa406b5ec868ed830c409c
fmlHappy <1308952869@qq.com>
2023-12-11 11:18:10 +0800

    新增了dao方法

--- Webtest/src/com/workit/dao/UserDao.java
+++ Webtest/src/com/workit/dao/UserDao.java
@@ -1,8 +1,20 @@
 package com.workit.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import com.workit.bean.UserBean;
+
 public class UserDao {
 	//登录
+	//查找所有
+	public List<UserBean> findall(int userid){
+		UserDao userdao = new UserDao();
+		List<UserBean>  luser = new ArrayList<UserBean>();
+		return luser;
+	}
 	//增加
+	
 	//修改
 	//删除
 
